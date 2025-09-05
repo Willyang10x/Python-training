e3b9b93ea4cdf2996076d3566cc4896fe721e0ad
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2025-09-05 10:35:21 -0300

    Lista 2 de exercicio

---
+++ lista-exercicios/lista2.py
@@ -0,0 +1,29 @@
+"""
+altura = float(input("Insira sua altura:"))
+
+centimetros = altura * 100
+
+print(f"Sua altura foi convertida em Centrimetros: {centimetros}")
+
+"""
+
+"""
+deslocamento = float(input("Insira o deslocamento do objeto em metros:"))
+
+variacaoTempoSegundo = 60
+
+velocidadeMedia = deslocamento / variacaoTempoSegundo
+
+print(f"A velocidade media em segundos do objeto é: {velocidadeMedia} segundos.")
+
+"""
+
+"""
+valor = float(input("Insira um valor:"))
+pi = 3.1415
+area  = pi* (valor ** 2)
+
+print(area)
+
+"""
+
