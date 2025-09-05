4718d71d0fceaea89fc1bb11bcdc124687fc982c
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2025-09-04 22:30:21 -0300

    Subindo lista

---
+++ lista-exercicios/questão.py
@@ -0,0 +1,65 @@
+"""
+numero = int(input("Insira um número:"))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** 0.5
+
+print(f"O dobro de {numero} é: {dobro}")
+print(f"O triplo de {numero} é: {triplo}")
+print(f"A raiz de {numero} é: {raiz}") 
+"""
+
+"""
+nome = input("Insira seu nome:")
+salario = float(input("Insira o valor do seu salario:"))
+aumento = salario * 0.10
+
+print(f"Olá {nome}, Seja bem vindo(a)\nSeu salario foi reajustado e agora é: {aumento+salario} ")
+
+"""
+
+"""
+idade = int(input("Insira sua idade:"))
+
+if idade < 18:
+    print("Você é menor de idade")
+elif idade >= 60:
+    print("Você é idoso")
+else:
+    print("Você é maior de idade")
+"""
+
+"""
+numero = int(input("Insira um número:"))
+
+if numero % 2 == 0:
+    print("Esse número é par")
+else:
+    print("Esse número é impar")
+"""
+
+"""
+for numero in range(1,21):
+    if numero % 2 == 0:
+        print(numero)
+"""
+
+"""
+numero = int(input("Insira um número:"))
+
+for i in range(1,11):
+    tabuada = numero * i
+    print(f"{numero} x {i}: {tabuada}")
+"""
+
+"""
+contador = 0
+while True :
+    numero = int(input("Insira um número:"))
+    if numero == 0:
+        break
+    contador += numero
+print(f"A soma dos número é {contador}")
+
+"""
