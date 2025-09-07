91a29809b9042bea9223314c80e6f548c4ccfc77
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2025-09-06 23:38:10 -0300

    Subindo lista 3

---
+++ lista-exercicios/lista3.py
@@ -0,0 +1,9 @@
+"""
+numero = int(input("Insira um número:"))
+
+if numero % 2 == 0:
+    print(f"Seu numero positivo e o resultado é: {numero**2}")
+else:
+    print(f"Seu numero é negativo e o resultado é: {numero**3}")
+"""
+
