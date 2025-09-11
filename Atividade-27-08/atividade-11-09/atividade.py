05b939c5a2020665fda8e202ae0b47710d79ad30
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2025-09-11 14:05:46 -0300

    Atividades-11-09

---
+++ Atividade-27-08/atividade-11-09/atividade.py
@@ -0,0 +1,41 @@
+"""
+numero = input("Insira um numero:")
+
+print(f"O numero informado foi {numero}")
+
+"""
+
+"""
+numero1 = int(input("Insira um numero:"))
+numero2 = int(input("Insira outro numero:"))
+
+numeros = numero1 + numero2
+
+print(f"A soma dos numero é {numeros}")
+"""
+
+"""
+nota1 = float(input("Insira sua 1 nota:"))
+nota2 = float(input("Insira sua 2 nota:"))
+nota3 = float(input("Insira sua 3 nota:"))
+nota4 = float(input("Insira sua 4 nota:"))
+
+notas = (nota1+nota2+nota3+nota4)/4
+
+print(f"A sua média é {notas}")
+"""
+
+"""
+tempCelsius = float(input("Insira uma temperatura em Celsius:"))
+
+tempF = (tempCelsius*9/5) + 32
+
+print(f"A temperatura convertida em fahrenheit é: {tempF}")
+"""
+
+"""
+tamanho_gb = float(input("Digite o tamanho do arquivo em GBs:"))
+tamanho_mb = tamanho_gb * 1_024
+tamanho_kb = tamanho_gb * 1_024 * 1_024
+print(f"O arquivo tem {tamanho_mb} MBs ou {tamanho_kb} Kbs")
+"""
