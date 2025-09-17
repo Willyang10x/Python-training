397b7f89b31b7ee669d96f16dcaa75796802b28d
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2025-09-17 16:25:18 -0300

    Atividade listas

---
+++ Atividade-27-08/atividade-11-09/atividade-lista17-09.py
@@ -0,0 +1,33 @@
+"""
+lista = ["Carolina", 1.70, 19]
+lista.append("Ana")
+
+print(f"Nome: {lista[0]}")
+print(f"Altura: {lista[1]}")
+print(f"Idade: {lista[2]}")
+print(f"Nome: {lista[3]}")
+"""
+
+"""
+atrizes = ["Paolão"]
+atrizes.append("Gabriel")
+
+while True:
+    nome = input("Digite seu nome:")
+    atrizes.append(nome)
+    resp = input("deseja continuar [S,N]?")
+    if resp.upper() == "N":
+        break
+print(atrizes)
+"""
+
+"""
+import random
+
+atrizes = ["Ana", "Julia", "juca"]
+random.shuffle(atrizes)
+print(f"Lista embaralhada: {atrizes}")
+sorteada = random.choice(atrizes)
+print(f"Atriz sorteada: {sorteada}")
+"""
+
