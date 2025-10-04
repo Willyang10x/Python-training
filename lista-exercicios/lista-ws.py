7556b8d9b126040d85037f65e10b0f757c9da2d9
Willyan Gabriel <145140640+Willyang10x@users.noreply.github.com>
2025-10-04 12:49:22 -0300

    lista ws completa

--- lista-exercicios/lista-ws.py
+++ lista-exercicios/lista-ws.py
@@ -28,15 +28,76 @@ if media >= 7:
 else:
     print(f"Você foi reprovado! com nota {media:.1f}")"""
 
-print("Ola, aproveite a mercadoria e boas compras!")
-nome = input("Insira o seu nome:")
-idade = int(input("Insira sua idade:"))
-if idade >=18:
-   print(f"Maior de idade. {idade}")
-else:
-   print(f"Menor de idade {idade}")
-endereco =input("Insira o seu endereco:")
-n = int(input("Insira o numero da sua residencia:"))
-escolha = input("EScolha seus produtos:")
+# print("Ola, aproveite a mercadoria e boas compras!")
+# nome = input("Insira o seu nome:")
+# idade = int(input("Insira sua idade:"))
+# if idade >=18:
+#    print(f"Maior de idade. {idade}")
+# else:
+#    print(f"Menor de idade {idade}")
+# endereco =input("Insira o seu endereco:")
+# n = int(input("Insira o numero da sua residencia:"))
+# escolha = input("EScolha seus produtos:")
 
-print(f"nome: {nome}\nidade: {idade}\nendereço:{endereco}\nnumero da rua: {n}\nSeus produtos:{escolha}\nCompras finalizdas! Aproveite sua feira e volte sempre." )
+# print(f"nome: {nome}\nidade: {idade}\nendereço:{endereco}\nnumero da rua: {n}\nSeus produtos:{escolha}\nCompras finalizdas! Aproveite sua feira e volte sempre." )
+
+# tempo = int(input())
+# horas = tempo // 3600
+# minutos = (tempo % 3600)//60
+# segundos = tempo % 60
+# print(f"{horas}:{minutos}:{segundos}") = int(input())
+
+# idade = int(input())
+
+# ano = idade // 365
+# mes = (idade % 365) // 30
+# dia = (idade % 365) % 30
+
+# print(f"{ano} ano(s)\n{mes} mes(es)\n{dia} dia(s)")
+
+
+# A, B, C, D = map(int, input().split())
+
+# if B > C and D > A and (C + D) > (A + B) and C > 0 and D > 0 and A % 2 == 0:
+#     print("Valores aceitos")
+# else:
+#     print("Valores nao aceitos")
+
+
+# valor = float(input())
+
+# if 0 <= valor <= 25:
+#     print("Intervalo [0,25]")
+# elif 25 < valor <= 50:
+#     print("Intervalo (25,50]")
+# elif 50 < valor <= 75:
+#     print("Intervalo (50,75]")
+# elif 75 < valor <= 100:
+#     print("Intervalo (75,100]")
+# else:
+#     print("Fora de intervalo")
+
+
+# n1, n2, n3, n4 = map(float, input().split())
+
+# media = (n1*2 + n2*3 + n3*4 + n4*1) / 10
+
+# print(f"Media: {media:.1f}")
+
+# if media >= 7.0:
+#     print("Aluno aprovado.")
+# elif media < 5.0:
+#     print("Aluno reprovado.")
+# else:
+#     print("Aluno em exame.")
+#     exame = float(input())
+#     print(f"Nota do exame: {exame:.1f}")
+    
+#     media_final = (media + exame) / 2
+
+#     if media_final >= 5.0:
+#         print("Aluno aprovado.")
+#     else:
+#         print("Aluno reprovado.")
+        
+#     print(f"Media final: {media_final:.1f}")
